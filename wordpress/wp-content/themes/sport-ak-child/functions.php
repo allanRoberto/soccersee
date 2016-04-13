946855e8ff966fb250adb2d2ea6983b1bec08ad7
allanroberto <allan.rsti@gmail.com>
2016-04-13 11:37:22 -0300

    Formulário incompleto

--- wordpress/wp-content/themes/sport-ak-child/functions.php
+++ wordpress/wp-content/themes/sport-ak-child/functions.php
@@ -10,6 +10,14 @@ function azexo_load_childtheme_languages() {
     load_child_theme_textdomain('sport-ak', get_stylesheet_directory() . '/languages');
 }
 
+
+add_action('wp_enqueue_scripts', 'azexo_custom_scripts');
+
+function azexo_custom_scripts() {
+    wp_register_script('custom-js', get_template_directory_uri() . '-child/js/custom.js', array('jquery'), AZEXO_THEME_VERSION, true);
+    wp_enqueue_script('custom-js');
+}
+
 /* Please add your custom functions code below this line. */
 
 
